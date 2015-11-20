c04b8e7b4f38f25f0b3f0dcb5e7b4866b61f57f2
amanda <amandayoung686@gmail.com>
2015-11-20 12:14:49 -0600

    Landing page problems

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,28 @@
-var blocJams = angular.module('blocJams', []);
+var blocJams = angular.module('blocJams', ['ui.router']);
+blocJams.controller('ExampleController', function($scope) {
+    $scope.someText = 'Hello World!';
+});
+blocJams.config(function($stateProvider, $locationProvider) {
+    
+    $locationProvider.html5Mode({
+        enabled:true,
+        requireBase: false
+    });
+    $stateProvider
+        .state('landing', {
+            url: '/',
+            controller: 'LandingCtrl',
+            templateUrl:'/templates/landing.html'
+    })
+        .state('collection', {
+            url:'/collection',
+            controller: 'collectionCtrl',
+            templateUrl:'/templates/collection.html'
+    })
+        .state('album', {
+            url:'/album',
+            controller:'albumCtrl',
+            templateUrl:'/templates/albumhtml'
+               
+    
+});
