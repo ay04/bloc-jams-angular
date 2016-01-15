73e4c11fa8f69ad1774d555ae98b2c6d6173cceb
amanda <amandayoung686@gmail.com>
2016-01-15 08:38:30 -0600

    Angular 3 Redo

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,28 +1,26 @@
-var blocJams = angular.module('blocJams', ['ui.router']);
-blocJams.controller('ExampleController', function($scope) {
-    $scope.someText = 'Hello World!';
-});
-blocJams.config(function($stateProvider, $locationProvider) {
-    
-    $locationProvider.html5Mode({
-        enabled:true,
-        requireBase: false
-    });
+(function () {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        
     $stateProvider
         .state('landing', {
             url: '/',
-            controller: 'LandingCtrl',
-            templateUrl:'/templates/landing.html'
-    })
-        .state('collection', {
-            url:'/collection',
-            controller: 'collectionCtrl',
-            templateUrl:'/templates/collection.html'
+            templateUrl: '/templates/landing/html'
     })
         .state('album', {
-            url:'/album',
-            controller:'albumCtrl',
-            templateUrl:'/templates/albumhtml'
-               
-    
-});
+            url: '/album', 
+            templateUrl: '/templates/album.html'
+    })
+        .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/collection.html'
+    });
+        
+angular
+    .module('blocJams', [ui.router])
+    .config(config);
+})();
